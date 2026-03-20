6fda7862677892869cdc7d0d5545952bdf74b5c8
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2026-03-20 18:52:03 -0400

    sql to create raw contacts table

---
+++ sql/raw/raw_contacts.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE NORTHLAKE_DB.RAW.raw_contacts( 
+    contact_id VARCHAR, 
+    account_id VARCHAR, 
+    first_name VARCHAR, 
+    last_name VARCHAR, 
+    email VARCHAR, 
+    job_title VARCHAR, 
+    role VARCHAR, 
+    phone VARCHAR, 
+    created_at VARCHAR, 
+    last_login VARCHAR, 
+    is_active VARCHAR
+);
