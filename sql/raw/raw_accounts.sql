18c99bb948600fdd133a6ca7063c77186f5da051
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2026-03-20 18:46:30 -0400

    sql to create raw accounts table

---
+++ sql/raw/raw_accounts.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE NORTHLAKE_DB.RAW.raw_accounts( 
+    account_id VARCHAR, 
+    company_name VARCHAR, 
+    industry VARCHAR, 
+    company_size VARCHAR, 
+    region VARCHAR, 
+    country VARCHAR, 
+    website VARCHAR, 
+    lead_source VARCHAR, 
+    created_at VARCHAR, 
+    updated_at VARCHAR, 
+    is_deleted VARCHAR
+);
