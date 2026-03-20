c14fad5ddf9b17972884ba5fc9a92f1afafdf543
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2026-03-20 18:37:03 -0400

    sql to create raw plans table

---
+++ sql/raw/raw_plans.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE TABLE NORTHLAKE_DB.RAW.raw_plans(
+    plan_id VARCHAR, 
+    plan_name VARCHAR, 
+    billing_cycle VARCHAR, 
+    mrr_usd VARCHAR, 
+    max_seats VARCHAR, 
+    max_api_calls VARCHAR
+);
